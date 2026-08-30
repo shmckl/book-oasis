49765199678299ff06551430538fbaa3ea00ebb6
Dmitriy Pak <dimapak75@gmail.com>
2026-08-30 23:40:58 +0100

    Initial commit

---
+++ src/main/java/com/bookoasis/book_oasis/BookOasisApplication.java
@@ -0,0 +1,13 @@
+package com.bookoasis.book_oasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOasisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookoasis/book_oasis/BookOasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookoasis.book_oasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
